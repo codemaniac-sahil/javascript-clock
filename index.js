887b391542a965255d546b7b07af4f5970e82b64
Sahil bisht <58036566+codemaniac-sahil@users.noreply.github.com>
2022-01-20 19:37:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const option={weekday:'long',year:'numeric',month:'long',day:'numeric'};
 setInterval(()=>
 {
     a=new Date();
+    if (a.getSeconds()<9){
+       a.getSeconds()= "0"+a.getSeconds() 
+    }
     
     time=a.getHours() +':'+ ''+a.getMinutes() + ':' + ''+ a.getSeconds();
     date=a.toLocaleDateString(undefined,option);
     document.getElementById('time').innerHTML=time +' '+ 'on'+' ' + date;
-    if (a.getSeconds()<9){
-       a.getSeconds()= "0"+a.getSeconds() 
-    }
+    
 },1000
 
 )
