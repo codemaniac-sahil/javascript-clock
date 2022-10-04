23094699c62f95d501bf84e28d3a3572c773caef
Aaryan Singh <94289067+AaryanSingh0017@users.noreply.github.com>
2022-10-04 14:56:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,163 @@
-let time;
-let date;
-let a;
-const option = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-setInterval(() => {
-    a = new Date();
-    s = a.getHours()
-    f = a.getSeconds()
-    if (f < 10) {
-        f = '0' + f;
-    }
-    if (s < 10) {
-        s = '0' + s;
-    }
-    m = a.getMinutes()
-    if (m < 10) {
-        m = '0' + m;
-    }
 
-    time = s + ':' + '' + m + ':' + '' + f;
-    date = a.toLocaleDateString(undefined, option);
-    document.getElementById('clock').innerHTML = time;
-    document.getElementById('day').innerHTML = date;
-}, 1000
 
-)
-a = new Date();
-s = a.getHours()
-if (s<12){
-    document.getElementById('time').innerHTML = " Morning";
-    document.body.style = "background-image:linear-gradient(yellow,orange);";
-}else if (s<16) {
-    document.getElementById('time').innerHTML = " Afternoon";
-    document.body.style = "background-image:linear-gradient(red,yellow);";
-}else{
-    document.getElementById('time').innerHTML = " Evening";
-    document.body.style = "background-image:linear-gradient(black,gray);";
+var sound = new Audio("https://www.freespecialeffects.co.uk/soundfx/animals/duck1.wav");
+		sound.loop = true;
+
+var h2 = document.getElementById('clock');
+
+// display current time by the second
+var currentTime = setInterval(function(){
+	var date = new Date();
+	
+	var hours = (12 - (date.getHours()));
+	// var hours = date.getHours();
+	
+	var minutes = date.getMinutes();
+	
+	var seconds = date.getSeconds();
+	
+	var ampm = (date.getHours()) < 12 ? 'AM' : 'PM';
+
+
+	//convert military time to standard time
+
+	if (hours < 0) {
+		hours = hours * -1;
+	} else if (hours == 00) {
+		hours = 12;
+	} else {
+		hours = hours;
+	}
+
+	
+	h2.textContent = addZero(hours) + ":" + addZero(minutes) + ":" + addZero(seconds) + "" + ampm;
+	
+},1000);
+
+
+/*functions to get hour, min, secs, 
+  am or pm, add zero, set alarm time and sound, clear alarm
+*/
+
+function addZero(time) {
+
+		return (time < 10) ? "0" + time : time;
+	
 }
+
+function hoursMenu(){
+
+	var select = document.getElementById('alarmhrs');
+	var hrs = 12
+
+	for (i=1; i <= hrs; i++) {
+		select.options[select.options.length] = new Option( i < 10 ? "0" + i : i, i);
+		
+	}
+}
+hoursMenu();
+
+function minMenu(){
+
+	var select = document.getElementById('alarmmins');
+	var min = 59;
+
+	for (i=0; i <= min; i++) {
+		select.options[select.options.length] = new Option(i < 10 ? "0" + i : i, i);
+	}
+}
+minMenu();
+
+function secMenu(){
+
+	var select = document.getElementById('alarmsecs');
+	var sec = 59;
+
+	for (i=0; i <= sec; i++) {
+		select.options[select.options.length] = new Option(i < 10 ? "0" + i : i, i);
+	}
+}
+secMenu();
+
+
+function alarmSet() {
+
+	var hr = document.getElementById('alarmhrs');
+	
+	var min = document.getElementById('alarmmins');
+	
+	var sec = document.getElementById('alarmsecs');
+	
+	var ap = document.getElementById('ampm');
+    
+
+    var selectedHour = hr.options[hr.selectedIndex].value;
+    var selectedMin = min.options[min.selectedIndex].value;
+    var selectedSec = sec.options[sec.selectedIndex].value;
+    var selectedAP = ap.options[ap.selectedIndex].value;
+
+    var alarmTime = addZero(selectedHour) + ":" + addZero(selectedMin) + ":" + addZero(selectedSec) + selectedAP;
+    console.log('alarmTime:' + alarmTime);
+
+    document.getElementById('alarmhrs').disabled = true;
+	document.getElementById('alarmmins').disabled = true;
+	document.getElementById('alarmsecs').disabled = true;
+	document.getElementById('ampm').disabled = true;
+
+
+//when alarmtime is equal to currenttime then play a sound
+	var h2 = document.getElementById('clock');
+
+/*function to calcutate the current time 
+then compare it to the alarmtime and play a sound when they are equal
+*/
+
+setInterval(function(){
+
+	var date = new Date();
+	
+	var hours = (12 - (date.getHours()));
+	// var hours = date.getHours();
+	
+	var minutes = date.getMinutes();
+	
+	var seconds = date.getSeconds();
+	
+	var ampm = (date.getHours()) < 12 ? 'AM' : 'PM';
+
+
+	//convert military time to standard time
+
+	if (hours < 0) {
+		hours = hours * -1;
+	} else if (hours == 00) {
+		hours = 12;
+	} else {
+		hours = hours;
+	}
+	
+	var currentTime = h2.textContent = addZero(hours) + ":" + addZero(minutes) + ":" + addZero(seconds) + "" + ampm;
+	
+
+	if (alarmTime == currentTime) {
+		sound.play();
+		}
+
+},1000);
+
+
+	// console.log('currentTime:' + currentTime);	
+
+}
+
+
+function alarmClear() {
+
+	document.getElementById('alarmhrs').disabled = false;
+	document.getElementById('alarmmins').disabled = false;
+	document.getElementById('alarmsecs').disabled = false;
+	document.getElementById('ampm').disabled = false;
+	sound.pause();
+}
+
 
