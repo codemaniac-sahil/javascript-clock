2853a36480dd7262ed46767bcc118ebcb9426dbc
Hackintosh-02 <97759154+Hackintosh-02@users.noreply.github.com>
2022-10-05 12:35:42 +0530

    Updated canges

--- script.js
+++ script.js
@@ -21,9 +21,7 @@ setInterval(() => {
     date = a.toLocaleDateString(undefined, option);
     document.getElementById('clock').innerHTML = time;
     document.getElementById('day').innerHTML = date;
-}, 1000
-
-)
+}, 1000)
 a = new Date();
 s = a.getHours()
 if (s<12){
@@ -37,3 +35,58 @@ if (s<12){
     // document.body.style = "background-image:linear-gradient(black,gray);";
 }
 
+// code for alarm
+
+hours = document.querySelector('#hours')
+mins = document.querySelector('#mins')
+
+for (i=0; i <= 23; i++) {
+    hours.options[hours.options.length] = new Option(i < 10 ? "0" + i : i, i);
+}
+for (i=0; i <= 59; i++) {
+    mins.options[mins.options.length] = new Option(i < 10 ? "0" + i : i, i);
+}
+
+function checkAlarm(alarmH,alarmM){
+    b = new Date()
+    checkHour = b.getHours()
+    checkMins = b.getMinutes()
+    if ((checkHour === parseInt(alarmH)) && (checkMins === parseInt(alarmM))) {
+        return true
+    } else {
+        return false
+    }
+}
+localStorage.setItem("isSet","false")
+setAlarm = document.querySelector('#setAlarm')
+setAlarm.addEventListener('click',()=>{
+    if (localStorage.isSet == "false"){
+    alarmHour = hours.value
+    alarmMinutes = mins.value
+    setAlarm.innerHTML = 'Reset'
+    localStorage.isSet = "true";
+    document.querySelector('.para').innerHTML = "Your alarm is set to " + alarmHour + " hrs : " + alarmMinutes + " mins"
+    const i = setInterval(()=>{
+        if (checkAlarm(alarmHour,alarmMinutes)){
+            var audio = new Audio('alarm1.mp3')
+            audio.play()
+            clearInterval(i)
+            hours.value = '00'
+            mins.value = '00'
+            document.querySelector('.para').innerHTML = 'Your alarm shows here'
+            setAlarm.innerHTML = 'Set Alarm'
+            localStorage.isSet = "false"
+            alert("Wake up !! Your alarm is ringing")
+        }
+    },1000)
+    z = i
+    } 
+    else {
+        localStorage.isSet = "false"
+        setAlarm.innerHTML = 'Set Alarm'
+        document.querySelector('.para').innerHTML = 'Your alarm shows here'
+        hours.value = '00'
+        mins.value = '00'
+        clearInterval(z);
+    }
+})
